335912ef8b7d61d2d60af2e94f5de5f1163503dd
ramos-vini <ramoscontrole@gmail.com>
2023-10-03 18:38:41 +0200

    PUT Method: updateQuestionById()

--- src/main/java/com/example/demo/survey/SurveyResource.java
+++ src/main/java/com/example/demo/survey/SurveyResource.java
@@ -2,9 +2,12 @@ package com.example.demo.survey;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -35,8 +38,12 @@ public class SurveyResource {
 
     @PostMapping("surveys/{surveyId}/questions")
     @ResponseStatus(HttpStatus.CREATED)
-    public void addQuestion(@PathVariable int surveyId, @RequestBody Question question){
+    public ResponseEntity<Object> addQuestion(@PathVariable int surveyId, @RequestBody Question question){
         surveyService.addQuestion(surveyId, question);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{questionId}").buildAndExpand(surveyService.getQuestionId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("surveys/{surveyId}/questions/{questionId}")
@@ -47,5 +54,16 @@ public class SurveyResource {
         if (question == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
         return question;
+    }
+
+    @DeleteMapping("surveys/{surveyId}/questions/{questionId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteQuestionById(@PathVariable int surveyId, @PathVariable int questionId){
+        surveyService.deleteQuestionById(surveyId, questionId);
+    }
+
+    @PutMapping("surveys/{surveyId}/questions/{questionId}")
+    public void updateQuestionById(@PathVariable int surveyId, @PathVariable int questionId, @RequestBody Question question){
+        if (!surveyService.updateQuestionById(surveyId, questionId, question)) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/example/demo/survey/SurveyService.java
+++ src/main/java/com/example/demo/survey/SurveyService.java
@@ -14,26 +14,36 @@ public class SurveyService {
     // Static "DB"
     private static List<Survey> surveys = new ArrayList<>();
 
-    private static int id = 0;
+    private static int lastSurveyId = 0;
+
+    private static int lastQuestionId = 0;
 
     static {
-        Question question1 = new Question(++id,
+        Question question1 = new Question(++lastQuestionId,
                 "Most Popular Cloud Platform Today", Arrays.asList(
                 "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "AWS");
-        Question question2 = new Question(++id,
+        Question question2 = new Question(++lastQuestionId,
                 "Fastest Growing Cloud Platform", Arrays.asList(
                 "AWS", "Azure", "Google Cloud", "Oracle Cloud"), "Google Cloud");
-        Question question3 = new Question(++id,
+        Question question3 = new Question(++lastQuestionId,
                 "Most Popular DevOps Tool", Arrays.asList(
                 "Kubernetes", "Docker", "Terraform", "Azure DevOps"), "Kubernetes");
 
         List<Question> questions = new ArrayList<>(Arrays.asList(question1,
                 question2, question3));
 
-        Survey survey = new Survey(1, "My Favorite Survey",
+        Survey survey = new Survey(++lastSurveyId, "My Favorite Survey",
                 "Description of the Survey", questions);
 
         surveys.add(survey);
+    }
+
+    int getSurveyId() {
+        return lastSurveyId;
+    }
+
+    int getQuestionId() {
+        return lastQuestionId;
     }
 
     // GET Methods
@@ -42,7 +52,7 @@ public class SurveyService {
         return surveys;
     }
 
-    public Survey getSurveyById(int surveyId) {
+    Survey getSurveyById(int surveyId) {
 
         Predicate<Survey> predicate = survey -> survey.getId() == surveyId;
 
@@ -58,15 +68,40 @@ public class SurveyService {
 
     Question getQuestionById(int surveyId, int questionId){
         List<Question> questions = getAllQuestionsBySurveyId(surveyId);
-        Optional<Question> optionalQuestion = questions.stream().filter(question -> question.getId() == questionId).findFirst();
+        Optional<Question> optionalQuestion = questions.stream().filter(q -> q.getId() == questionId).findFirst();
         return optionalQuestion.orElse(null);
     }
 
     // POST Methods
 
-    public void addQuestion(int surveyId, Question question) {
+    void addQuestion(int surveyId, Question question) {
         Survey survey = getSurveyById(surveyId);
-        question.setId(++id);
+        question.setId(++lastQuestionId);
         survey.getQuestions().add(question);
+    }
+
+    void addQuestionKeepId(int surveyId, int questionId, Question question) {
+        Survey survey = getSurveyById(surveyId);
+        question.setId(questionId);
+        survey.getQuestions().add(question);
+    }
+
+    // DELETE Methods
+
+    public boolean deleteQuestionById(int surveyId, int questionId) {
+        List<Question> questions = getAllQuestionsBySurveyId(surveyId);
+
+        return questions.removeIf(question -> question.getId() == questionId);
+    }
+
+    // PUT Methods
+
+    public boolean updateQuestionById(int surveyId, int questionId, Question question) {
+        boolean exists = deleteQuestionById(surveyId, questionId);
+
+        if (!exists) return false;
+
+        addQuestionKeepId(surveyId, questionId, question);
+        return true;
     }
 }
