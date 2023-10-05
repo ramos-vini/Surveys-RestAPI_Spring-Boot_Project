dd90876be6f201264a418b4b67078d1a5cc2ea75
ramos-vini <ramoscontrole@gmail.com>
2023-10-05 13:58:58 +0200

    Spring Boot Starter Rest

--- src/main/java/com/example/demo/user/UserCommandLineRunner.java
+++ src/main/java/com/example/demo/user/UserCommandLineRunner.java
@@ -1,7 +1,5 @@
 package com.example.demo.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
